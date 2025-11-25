f35c3adadcd8383903a1849efd767723321ae6b4
Alfredo Antonio Diaz Claro <alfredoa.diazc@gmail.com>
2025-11-25 16:33:06 -0500

    arreglo

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -7,9 +7,12 @@ function Navbar() {
     <nav className="navbar">
       <h1 className="logo">🌱 SembrandoBits</h1>
       <ul className="nav-links">
-        <li><Link to="/
-        ">Home</Link></li>
-          <li><Link to="/sensores">Sensores</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/sensores">Sensores</Link>
+        </li>
       </ul>
     </nav>
   );
--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import Home from "./Pages/home/Home";       // 👈 cambiado
+import Home from "./Pages/home/Home";
 import Cultivos from "./Pages/cultivos/Cultivos";
 import TipoTierra from "./Pages/Tierras/TipoTierra";
 import Sensores from "./Pages/Sensores/Sensores";
@@ -11,13 +11,13 @@ import Sensores from "./Pages/Sensores/Sensores";
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
+      <div className="app-container">
         <Navbar />
-        
-        <main className="flex-grow p-6 bg-gray-50">
+
+        <main className="main-content">
           <Routes>
-            <Route path="/" element={<Home />} />          {/* 👈 cambiado */}
-              <Route path="/Sensores" element={<Sensores />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/sensores" element={<Sensores />} />
           </Routes>
         </main>
 
